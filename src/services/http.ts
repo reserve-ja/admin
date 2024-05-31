acba1d251c1b1d86580ea687905aff341afde26f
Mario Simão <mariosimao@poli.ufrj.br>
2024-05-31 19:23:18 -0300

    feat: increase API request timeout

--- src/services/http.ts
+++ src/services/http.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export const http = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  timeout: 2000,
+  timeout: 10_000,
 })
 
 http.interceptors.request.use((config) => {
