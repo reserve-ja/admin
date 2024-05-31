a64d5d24279853da8180c3f8de3961e88ce40769
Mario Simão <mariosimao@poli.ufrj.br>
2024-05-31 19:34:20 -0300

    chore: add custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/admin/',
   plugins: [
     vue({
       template: { transformAssetUrls }
